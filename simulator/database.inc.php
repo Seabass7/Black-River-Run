d9506b5fd794517cfcfd13e0957af4e39f3cd263
Sebastian Oveland <Seabass7@users.noreply.github.com>
2019-01-24 16:04:09 +0100

    Update database connection

--- simulator/database.inc.php
+++ simulator/database.inc.php
@@ -5,11 +5,10 @@
         // Try to connect to the database.
         try {
             $db = new PDO(
-                "mysql:host=".$dbhost.";dbname=".$dbname, 
-                $dbusername, 
-                $dbpassword, 
-                array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8")
-            );
+			    "mysql:host=".$dbhost.";dbname=".$dbname.";charset=utf8mb4",
+			    $dbusername, 
+			    $dbpassword
+		    );
         } catch(PDOexception $e) {
             die("Error while trying to connect to the database...");
         }
